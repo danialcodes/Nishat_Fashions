ae51cbbdacc191c7a309890e7ff0a41c7995fc4e
danialcodes <md.danial.islam@g.bracu.ac.bd>
2022-06-19 21:31:31 +0600

    navbar and footer added also some retouch in code

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// This folder is  used basically for using tailwind autocompletion extension
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
